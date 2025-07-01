82eccb963f1da8d1fb7c3f7d705051849b8e1337
Pog Coin <info.pogcoin@gmail.com>
2025-07-01 22:47:37 +0000

    ConsoleError: React.jsx: type is invalid -- expected a string (for built

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,3 +1,4 @@
+
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
@@ -30,12 +31,15 @@ const CardHeader = React.forwardRef<
 CardHeader.displayName = "CardHeader"
 
 const CardTitle = React.forwardRef<
-  HTMLHeadingElement,
+  HTMLParagraphElement,
   React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
   <h3
     ref={ref}
-    className={cn("text-2xl font-semibold leading-none tracking-tight", className)}
+    className={cn(
+      "text-2xl font-semibold leading-none tracking-tight",
+      className
+    )}
     {...props}
   />
 ))
